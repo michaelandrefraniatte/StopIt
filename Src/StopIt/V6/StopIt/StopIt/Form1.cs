cbf75f99d92edf781164e20a1a671c8e885fc962
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-02 16:30:50 +0100

    Add about

--- Src/StopIt/V6/StopIt/StopIt/Form1.cs
+++ Src/StopIt/V6/StopIt/StopIt/Form1.cs
@@ -45,6 +45,23 @@ namespace StopIt
         private static bool edgechecking = false;
         private static int edgenumber = 0, edgenumbertemp = 0;
         private static bool closed = false, closeonicon = false;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        private void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
         private void Form1_Shown(object sender, EventArgs e)
         {
             using (StreamWriter createdfile = new StreamWriter(Application.StartupPath + @"\temphandle"))
